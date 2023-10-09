dd6b3c71b0b58ea2bb7fe7d7abbc109faa735dd5
Traister101 <66151907+Traister101@users.noreply.github.com>
2023-10-09 01:08:14 -0700

    Recipes show up in Jei. Requires we have a JEI plugin so in addition to the required build.gradle changes the build file has been revamped in general.
    All TFC recipes that are registered in code have been consolidated to RegisterRecipes.java as we also needed to register a heat recipe for firing of our mold

--- src/main/java/com/buuz135/hotornot/HotOrNot.java
+++ src/main/java/com/buuz135/hotornot/HotOrNot.java
@@ -39,9 +39,9 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = HotOrNot.MOD_ID, name = HotOrNot.MOD_NAME, version = HotOrNot.VERSION, useMetadata = true)
 public class HotOrNot {
 
-	public static final String MOD_ID = "hotornot";
-	public static final String MOD_NAME = "Hot Or Not - TFC";
-	public static final String VERSION = "1.1.6";
+	public static final String MOD_ID = "${MODID}";
+	public static final String MOD_NAME = "${MODNAME}";
+	public static final String VERSION = "${VERSION}";
 
 	public static final CreativeTabs HOTORNOT_TAB = new HotOrNotTab();
 
